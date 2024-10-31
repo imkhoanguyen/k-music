75b696db8e00ce825d16612690fa10a11b173721
khoa <khoanguyen.coder@gmail.com>
2024-10-31 07:35:37 +0700

    refactor: add song routing

--- client/src/app/features/admin/admin-routing.module.ts
+++ client/src/app/features/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GenreComponent } from './genre/genre.component';
 import { SingerComponent } from './singer/singer.component';
+import { SongComponent } from './song/song.component';
 
 // chú ý thứ tự để routing
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: '',
     component: AdminLayoutComponent,
     children: [
+      { path: 'song', component: SongComponent },
       { path: 'singer', component: SingerComponent },
       { path: 'genre', component: GenreComponent },
       { path: '', component: DashboardComponent },
