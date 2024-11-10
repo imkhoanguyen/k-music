eaa9e90ca94da7f11ff84510bce8e8578643efde
khoa <khoanguyen.coder@gmail.com>
2024-11-10 22:21:52 +0700

    refactor: add route login

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ServerErrorComponent } from './shared/component/server-error/server-error.component';
 import { NotFoundComponent } from './shared/component/not-found/not-found.component';
+import { LoginComponent } from './shared/component/login/login.component';
 
 export const routes: Routes = [
   {
@@ -13,6 +14,7 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./features/client/client.module').then((m) => m.ClientModule),
   },
+  { path: 'login', component: LoginComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
