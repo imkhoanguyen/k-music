d87752d105eddc15bbd0de52a7afe02876b815bf
khoa <khoanguyen.coder@gmail.com>
2024-11-23 11:59:18 +0700

    refactor: re-run migration

--- KM.Infrastructure/Migrations/20241123040131_initDb.Designer.cs
+++ KM.Infrastructure/Migrations/20241123040131_initDb.Designer.cs
@@ -4,6 +4,7 @@ using KM.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KM.Infrastructure.Migrations
 {
     [DbContext(typeof(MusicContext))]
-    partial class MusicContextModelSnapshot : ModelSnapshot
+    [Migration("20241123040131_initDb")]
+    partial class initDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
