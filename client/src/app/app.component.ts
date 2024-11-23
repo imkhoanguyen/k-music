aad3444d012ab092dbc4da54dc5c1d15969420e0
khoa <khoanguyen.coder@gmail.com>
2024-11-23 11:54:50 +0700

    feat: set currentt user when load

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  constructor(private router: Router) {}
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.authService.setCurrentUser(user);
+  }
 
   isAdminRoute(): boolean {
     // Check if the current URL starts with '/admin'
