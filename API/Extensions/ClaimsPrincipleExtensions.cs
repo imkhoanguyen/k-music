052580506ea6105a5bd765f86caf46a8a41edb47
khoa <khoanguyen.coder@gmail.com>
2024-11-23 11:55:56 +0700

    feat: add claims principle extension

---
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,24 @@
+﻿using KM.Domain.Exceptions;
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipleExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            var username = user.FindFirstValue(ClaimTypes.Name)
+                ?? throw new BadRequestException("Cannot get username from token");
+
+            return username;
+        }
+
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            var userId = user.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? throw new BadRequestException("Cannot get username from token");
+
+            return userId;
+        }
+    }
+}
