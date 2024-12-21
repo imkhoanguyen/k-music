9859f744fafac461acd91f3e2e74201bcabc1e53
Anh Khoa <khoanguyen.coder@gmail.com>
2024-12-21 08:48:57 +0700

    add vippackage endpoint

---
+++ API/Controllers/VipPackageController.cs
@@ -0,0 +1,62 @@
+﻿using KM.Application.DTOs.VipPackages;
+using KM.Application.Service.Abstract;
+using KM.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class VipPackageController : BaseApiController
+    {
+        private readonly IVipPackageService _vipPackageService;
+
+        public VipPackageController(IVipPackageService vipPackageService)
+        {
+            _vipPackageService = vipPackageService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VipPackage>>> GetAll()
+        {
+            var list = await _vipPackageService.GetAllAsync(vp => vp.IsDelete == false);
+            return Ok(list);
+        }
+
+        [HttpGet("{int:id}")]
+        public async Task<ActionResult<VipPackage>> GetVipPackage(int id)
+        {
+            return await _vipPackageService.GetAsync(vp => vp.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VipPackage>> Create(VipPackageCreateDto dto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var vipPackage = await _vipPackageService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetVipPackage), new { id = vipPackage.Id }, vipPackage);
+        }
+
+        [HttpPut("{int:id}")]
+        public async Task<ActionResult<VipPackage>> Update(int id, VipPackage entity)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var vipPackage = await _vipPackageService.UpdateAsync(id, entity);
+            return Ok(vipPackage);
+        }
+
+        [HttpDelete("{int:id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _vipPackageService.DeleteAsync(vp => vp.Id == id);
+            return NoContent();
+        }
+
+    }
+}
--- KM.Domain/Entities/VipPackage.cs
+++ KM.Domain/Entities/VipPackage.cs
@@ -1,11 +1,17 @@
-﻿namespace KM.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KM.Domain.Entities
 {
     public class VipPackage : BaseEntity
     {
+        [Required(ErrorMessage = "Tên gói đăng ký không được trống")]
         public required string Name { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "Giá bán phải lớn hơn 0")]
         public decimal Price { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "Giá sau khi giảm phải lớn hơn 0")]
         public decimal PriceSell { get; set; }
         public int DurationDay { get; set; }
+        [Required(ErrorMessage = "Mô tả gói đăng ký không được trống")]
         public required string Description { get; set; }
         public bool IsDelete { get; set; } = false;
     }
