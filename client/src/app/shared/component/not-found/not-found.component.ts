c8e2be4e850806d84c9d10fe87754dc33506f689
Anh Khoa <khoanguyen.coder@gmail.com>
2025-01-07 09:14:45 +0700

    style notfound component

--- client/src/app/shared/component/not-found/not-found.component.ts
+++ client/src/app/shared/component/not-found/not-found.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzResultModule } from 'ng-zorro-antd/result';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [NzResultModule, NzButtonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {
-  error?: any;
-
-  constructor(private router: Router) {
-    const navigation = this.router.getCurrentNavigation();
-    this.error = navigation?.extras.state?.['error'];
-  }
-}
+export class NotFoundComponent {}
