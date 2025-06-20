1815bc55c2c3fdddf8c80d85584ac5aac7a95f27
imkhoanguyen <khoanguyen.coder@gmail.com>
2025-06-20 23:21:40 +0700

    add result

---
+++ src/Music.Core/Common/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace Music.Core.Common
+{
+    public class Result
+    {
+        public bool Success { get; }
+        public string Error { get; }
+
+        protected Result(bool success, string error)
+        {
+            Success = success;
+            Error = error;
+        }
+
+        public static Result Ok()
+        {
+            return new Result(true, null);
+        }
+
+        public static Result<TValue> Ok<TValue>(TValue value)
+        {
+            return new Result<TValue>(value, true, null);
+        }
+
+        public static Result<TValue> Fail<TValue>(string error)
+        {
+            return new Result<TValue>(default(TValue), false, error);
+        }
+    }
+}
